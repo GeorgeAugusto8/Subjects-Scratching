f29d90027f47f882826ffe5d8f97a8eaba2be911
GeorgeAugusto8 <georgeaugusto.marcos@gmail.com>
2021-03-09 11:01:24 -0300

    resolvendo problema config gitignore

--- src/app.js
+++ src/app.js
@@ -2,14 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-const config = require('./config')
-
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-mongoose.connect(config.connectionString)
+mongoose.connect(process.env.DATABASE_URI)
 
 const Professor = require('./models/professor')
 const Faculdade = require('./models/faculdade')
